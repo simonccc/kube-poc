1f0e150f75f7b4d40fcc37df70d8b994468d78d4
simonccc <simonc@gmail.com>
2024-06-29 22:40:13 +0100

    test

--- main.tf
+++ main.tf
@@ -13,3 +13,58 @@ provider "proxmox" {
 }
 
 
+resource "proxmox_vm_qemu" "blog_demo_test" {
+  name = "test_vm${count.index + 1}" # count.index starts at 0
+  #name = "test-vm-01"
+  count = 1 # Establishes how many instances will be created 
+  target_node = var.proxmox_host
+
+  # References our vars.tf file to plug in our template name
+  clone = var.template_name
+  # Creates a full clone, rather than linked clone 
+  # https://pve.proxmox.com/wiki/VM_Templates_and_Clones
+  full_clone  = "true"
+
+  # VM Settings. `agent = 1` enables qemu-guest-agent
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disk {
+    slot = 0
+    size = "50G"
+    type = "scsi"
+    storage = "intel-storage" # Name of storage local to the host you are spinning the VM up on
+    # Enables SSD emulation
+    ssd = 1
+    # Enables thin-provisioning
+    discard = "on"
+    #iothread = 1
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.nic_name
+    tag = var.vlan_num # This tag can be left off if you are not taking advantage of VLANs
+  }
+
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  #provisioner "local-exec" {
+    # Provisioner commands can be run here.
+    # We will use provisioner functionality to kick off ansible
+    # playbooks in the future
+    #command = "touch /home/tcude/test.txt"
+  #}
+}
+
+
